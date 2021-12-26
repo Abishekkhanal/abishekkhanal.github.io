fbb0cf889469581d19e03fb8684729f942afbdd3
Abishek Khanal <90846785+Abishekkhanal@users.noreply.github.com>
2021-12-26 17:58:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -71,3 +71,16 @@ $(document).ready(function(){
         }
     });
 });
+//visitor count
+
+const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+	fetch('https://api.countapi.xyz/update/florin-popcom/codepen/?amount=1')
+	.then(res => res.json())
+	.then(res => {
+		countEl.innerHTML = res.value;
+	})
+}
